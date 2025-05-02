d831d9079ca0206c342feebe971433d0a4021da0
dobrevd <dobrev@ukr.net>
2025-05-02 10:51:01 +0200

    test is corrected

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-project');
   });
 
-  it('should render title', () => {
+  it('should contain router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-project');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
